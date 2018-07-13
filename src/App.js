cfc32b636f7a7ee2a6d9b04efa31589d32e69982
scott <you@example.comitsmescottb123@gmail.com>
2018-07-13 14:58:46 -0400

    initial commit for messaging app

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import { Home } from './components/screens'
+
+const App = props => {
+  return <Home />
+}
+
+export default App
---
+++ src/config/index.js
@@ -0,0 +1,3 @@
+export default {
+  turboAppId: "5b48f36557f04d00141de4cc"
+}
